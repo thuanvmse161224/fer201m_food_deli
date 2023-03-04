65cb2881060b970d510b573469690d366418420f
thuanvmse161224 <thuanvmse161224@fpt.edu.vn>
2023-03-04 23:18:30 +0700

    update scaffold for whole project

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 function App() {
     const [count, setCount] = useState(0);
@@ -8,6 +9,13 @@ function App() {
     return (
         <>
             <Header />
+            <Routes>
+                <Route path="/" element={<div><p>Hello World</p></div>} /> {/*Thay đổi element tùy ý*/}
+                <Route
+                    path="*"
+                    element={<Navigate to="/" replace />}
+                />
+            </Routes>
             <Footer />
         </>
     );
