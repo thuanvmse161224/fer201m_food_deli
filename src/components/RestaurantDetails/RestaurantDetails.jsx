8658945d8cedc28e534c0912dc7e65797998c39d
thuanvmse161224 <thuanvmse161224@fpt.edu.vn>
2023-03-12 21:25:09 +0700

    new branch ....

--- src/components/RestaurantDetails/RestaurantDetails.jsx
+++ src/components/RestaurantDetails/RestaurantDetails.jsx
@@ -1,7 +1,7 @@
 export default function RestaurantDetails() {
     return(
         <>
-        
+            <p>This page hasn't been implement yet. Sorry for your inconvenience</p>
         </>
     )
 }
