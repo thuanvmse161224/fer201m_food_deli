bf2f231ddf4a4044d21c234ecc474054be87350a
thuanvmse161224 <thuanvmse161224@fpt.edu.vn>
2023-03-12 21:27:05 +0700

    new branch ...

--- src/components/CategoriesPage/CategoryPage.jsx
+++ src/components/CategoriesPage/CategoryPage.jsx
@@ -1,7 +1,7 @@
 export default function CategoryPage() {
     return(
         <>
-        
+            <p>This page hasn't been implement yet. Sorry for your inconvenience</p>
         </>
     )
 }
