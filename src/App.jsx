1354792167971aa49a98705966a7f014ec65dfd6
autobot-tm <autobot.tm19@gmail.com>
2023-03-13 17:48:29 +0700

    update category

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,31 @@ import HomeSale from "./components/HomeSale/HomeSale";
 import { height } from "@mui/system";
 import Contact from "./components/Contact/Contact";
 
-
 function App() {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    return (
-        <>
-            <Header />
-            <Routes>
-
-                <Route path="/" element={<div style={{height: '1000px'}}><p></p></div>} />{/* Thay đổi element tùy ý */}
-                <Route path="/" element={<Contact />} /> {/*Thay đổi element tùy ý*/}
-                <Route path="/contact" element={<Contact />} />
-
-                <Route
-                    path="*"
-                    element={<Navigate to="/" replace />}
-                />
-            </Routes>
-            <HomeSale/>
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div style={{ height: "1000px" }}>
+              <p></p>
+            </div>
+          }
+        />
+        {/* Thay đổi element tùy ý */}
+        <Route path="/" element={<Contact />} /> {/*Thay đổi element tùy ý*/}
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="/catePage" element={<CategoryPage />} />
+      </Routes>
+      <HomeSale />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
