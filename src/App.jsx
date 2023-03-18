9feb7d45083ba2e49c487ef5c18a76b17acc9320
QKL <qklphepha@gmail.com>
2023-03-18 12:40:16 +0700

    Create LoginPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,44 @@
-  import { useState } from "react";
-  import Header from "./components/Header/Header";
-  import Footer from "./components/Footer/Footer";
-  import { Navigate, Route, Routes } from "react-router-dom";
+import { useState } from "react";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import { Navigate, Route, Routes } from "react-router-dom";
 
-  import HomeSale from "./components/HomeSale/HomeSale";
-  import { height } from "@mui/system";
-  import Contact from "./components/Contact/Contact";
-  import CategoryPage from "./components/CategoriesPage/CategoryPage";
-  import HeaderFixed from "./components/Header/HeaderFixed";
-  import HomeMenu from "./components/HomeMenu/HomeMenu";
-  function App() {
-    const [count, setCount] = useState(0);
+import HomeSale from "./components/HomeSale/HomeSale";
+import { height } from "@mui/system";
+import Contact from "./components/Contact/Contact";
+import CategoryPage from "./components/CategoriesPage/CategoryPage";
+import HeaderFixed from "./components/Header/HeaderFixed";
+import HomeMenu from "./components/HomeMenu/HomeMenu";
+import LoginPage from "./components/LoginPage/LoginPage";
 
-    return (
-      <>
-        <HeaderFixed />
-       
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-               <Header />
-                <HomeSale />
-                <HomeMenu />
-              </>
-            }
-          />
-          {/* Thay đổi element tùy ý */}
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/catePage/:cateName" element={<CategoryPage />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
+function App() {
+  const [count, setCount] = useState(0);
 
-        <Footer />
-      </>
-    );
-  }
+  return (
+    <>
+      <HeaderFixed />
 
-  export default App;
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <HomeSale />
+              <HomeMenu />
+            </>
+          }
+        />
+        {/* Thay đổi element tùy ý */}
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/catePage/:cateName" element={<CategoryPage />} />
+        <Route path="/loginPage" element={<LoginPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
